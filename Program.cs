237d6fc4154e2f15ed4937a4fdcc4a0b44560f55
Martin Randall <martin@coryl.com>
2021-02-02 13:02:07 +0000

    Added hardest ever grid created

--- Program.cs
+++ Program.cs
@@ -49,8 +49,26 @@ var evilGrid = new Grid(new[]
     0, 0, 0, 0, 3, 0, 1, 0, 2,
     9, 0, 0, 0, 0, 0, 0, 5, 0
 });
+var hardestEverGrid = new Grid(new[]
+{
+    0, 0, 5, 3, 0, 0, 0, 0, 0,
+    8, 0, 0, 0, 0, 0, 0, 2, 0,
+    0, 7, 0, 0, 1, 0, 5, 0, 0,
+    4, 0, 0, 0, 0, 5, 3, 0, 0,
+    0, 1, 0, 0, 7, 0, 0, 0, 6,
+    0, 0, 3, 2, 0, 0, 0, 8, 0,
+    0, 6, 0, 5, 0, 0, 0, 0, 9,
+    0, 0, 4, 0, 0, 0, 0, 3, 0,
+    0, 0, 0, 0, 0, 9, 7, 0, 0
+});
 var grids = new[]
-    {("Easy Grid", easyGrid), ("Medium Grid", mediumGrid), ("Hard Grid", hardGrid), ("Evil Grid", evilGrid)};
+{
+    ("Easy Grid", easyGrid),
+    ("Medium Grid", mediumGrid),
+    ("Hard Grid", hardGrid), 
+    ("Evil Grid", evilGrid),
+    ("Hardest Ever", hardestEverGrid)
+};
 var solver = new GridSolver();
 foreach (var grid in grids)
 {
